60cb6f050624eff5fd0623b15619b7161304abe7
Ayush <akjayushkumarjha8@gmail.com>
2026-04-16 19:56:27 +0530

    fix-superuser

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
-python superuser.py
+python accounts/superuser.py
 python manage.py collectstatic --noinput
