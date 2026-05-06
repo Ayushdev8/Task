c8f78395238f2182d8b18d0d3cd841549006e660
Ayush <akjayushkumarjha8@gmail.com>
2026-05-06 16:14:34 +0530

    miatrapupdate

--- taskmanage/settings.py
+++ taskmanage/settings.py
@@ -183,6 +183,6 @@ ANYMAIL = {
   "MAILTRAP_API_TOKEN": config('MAILTRAP_API_TOKEN'),
 }
 EMAIL_BACKEND = "anymail.backends.mailtrap.EmailBackend"
-DEFAULT_FROM_EMAIL = "hello@taskmanagerfront-eight.vercel.app"
+DEFAULT_FROM_EMAIL = "hello@demomailtrap.co"
 
 MAILTRAP_TEMPLATE_UUID=config('MAILTRAP_TEMPLATE_UUID')
